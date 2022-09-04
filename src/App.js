d1a31c2261ef630c15612a3c74f3700b1fd953c8
vladislav <vladislav.frint@gmail.com>
2022-09-04 17:06:59 +0300

    add users

--- src/App.js
+++ src/App.js
@@ -2,14 +2,52 @@ import React from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [invites, setInvites] = useState([]);
+  const [success, setSuccess] = useState(false)
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites(prev => prev.filter(_id => _id !== id));
+    } else {
+      setInvites(prev => [...prev, id])
+    }
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('https://reqres.in/api/users')
+      .then(res => res.json())
+      .then(data => setUsers(data.data))
+      .catch(err => alert('Произошла ошибка' + err))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const onClickSendSuccess = () => {
+    setSuccess(true);
+  }
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {
+        success ? <Success count={invites.length}/> :
+          (
+            <Users
+              items={users}
+              isLoading={isLoading}
+              invites={invites}
+              onClickInvite={onClickInvite}
+              onClickSendSuccess={onClickSendSuccess}
+            />)
+      }
+
     </div>
   );
 }
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Skeleton } from './Skeleton';
 import { User } from './User';
 
-export const Users = ({ items, isLoading }) => {
+export const Users = ({ 
+  items, 
+  isLoading,
+  onClickInvite,
+  invites,
+  onClickSendSuccess,
+
+}) => {
+  const [searchValue, setSearchValue] = useState('');
+
+  const onChangeInput = (value) => {
+    setSearchValue(value);
+  };
+
+  const filteredItems = () => {
+    return items.filter((item => {
+      if (item.email.toLowerCase().includes(searchValue.toLowerCase())) {
+        return item;
+      } else if ((item['first_name'].toLowerCase() + ' ' + item['last_name'].toLowerCase()).includes(searchValue.toLowerCase())) {
+        return item;
+      }
+    }));
+  }
+
   return (
     <>
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+        <input onChange={(evt) => onChangeInput(evt.target.value)} type="text" placeholder="Найти пользователя..." value={searchValue} />
       </div>
       {isLoading ? (
         <div className="skeleton-list">
@@ -19,10 +42,21 @@ export const Users = ({ items, isLoading }) => {
         </div>
       ) : (
         <ul className="users-list">
-          <User />
+          {filteredItems().map((item, index) => (
+            <User
+              key={index}
+              email={item.email}
+              firstName={item['first_name']}
+              lastName={item['last_name']}
+              avatar={item.avatar}
+              isInvited={invites.includes(item.id)}
+              id={item.id}
+              onClickInvite={onClickInvite}
+            />
+          ))}
         </ul>
       )}
-      <button className="send-invite-btn">Отправить приглашение</button>
+      {invites.length > 0 ? <button onClick={onClickSendSuccess} className="send-invite-btn">Отправить приглашение</button> : null}
     </>
   );
 };
